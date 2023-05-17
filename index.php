b6d3bfe52a2839d2ea28392486f9cf82f914870b
abdul-rehman2709 <131893594+abdul-rehman2709@users.noreply.github.com>
2023-05-17 18:42:15 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Simple Page</title>
+</head>
+<body>
+    <h1>Welcome to my PHP application!</h1>
+    
+    <?php
+    // PHP code can be written here
+    $name = "PHP User";
+    echo "Hello, $name!";
+    ?>
+    
+    <p>This is a basic PHP page.</p>
+</body>
+</html>
